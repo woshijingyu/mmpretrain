744ac95ca0ded1fba5ccca61d589def2b289a474
woshijingyu <woshijingyu@users.noreply.github.com>
2023-09-12 23:33:17 +0800

    Add files via upload
    Added config file for fine tuning.

---
+++ finetune_config.py
@@ -0,0 +1,38 @@
+_base_ = [
+    'configs/_base_/models/resnet50.py',           # model settings
+    'configs/_base_/datasets/imagenet_bs32.py',    # data settings
+    'configs/_base_/schedules/imagenet_bs256.py',  # schedule settings
+    'configs/_base_/default_runtime.py',           # runtime settings
+]
+
+# model settings
+model = dict(
+    backbone=dict(
+        frozen_stages=2,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='https://download.openmmlab.com/mmclassification/v0/resnet/resnet18_8xb32_in1k_20210831-fbbb1da6.pth',
+            prefix='backbone',
+        )),
+    head=dict(num_classes=2),
+)
+
+# >>>>>>>>>>>>>>> Override data settings here >>>>>>>>>>>>>>>>>>>
+data_root = 'data/shoe'
+train_dataloader = dict(
+    dataset=dict(
+        type='CustomDataset',
+        data_root=data_root,
+        ann_file='',       # We assume you are using the sub-folder format without ann_file
+        data_prefix='train',
+    ))
+val_dataloader = dict(
+    dataset=dict(
+        type='CustomDataset',
+        data_root=data_root,
+        ann_file='',       # We assume you are using the sub-folder format without ann_file
+        data_prefix='val',
+    ))
+test_dataloader = val_dataloader
+checkpoint_config = dict(interval=10)
+# <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
