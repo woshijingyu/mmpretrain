2b447abb9b7ae04d15933004671444d7cbb0e648
woshijingyu <woshijingyu@gmail.com>
2023-09-27 23:02:19 +0800

    added train_cfg into custom config file

--- configs/_base_/schedules/imagenet_bs256.py
+++ configs/_base_/schedules/imagenet_bs256.py
@@ -7,7 +7,7 @@ param_scheduler = dict(
     type='MultiStepLR', by_epoch=True, milestones=[30, 60, 90], gamma=0.1)
 
 # train, val, test setting
-train_cfg = dict(by_epoch=True, max_epochs=100, val_interval=10)
+# train_cfg = dict(by_epoch=True, max_epochs=100, val_interval=10)
 val_cfg = dict()
 test_cfg = dict()
 
